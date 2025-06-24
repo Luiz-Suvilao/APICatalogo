e09bfb2520b58df5fbcfee443836620414dc76ae
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-06-23 22:15:34 -0300

    feat: Configure database connection

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,8 +1,14 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var dbConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApiCatalogoContext>(options => options.UseMySql(dbConnection, ServerVersion.AutoDetect(dbConnection)));
 
 var app = builder.Build();
 
