eca78e35b8c626b423d88b6cbed711e293a99274
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-06-23 22:10:25 -0300

    feat: Configure DbContext

---
+++ APICatalogo/Context/ApiCatalogoContext.cs
@@ -0,0 +1,10 @@
+﻿using APICatalogo.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class ApiCatalogoContext(DbContextOptions<ApiCatalogoContext> options) : DbContext(options)
+{
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+}
