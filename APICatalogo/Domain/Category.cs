d62e4704fa279b52142262d5a3b376e6ded747c0
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2026-06-01 21:54:28 -0300

    feat: add validation attributes and update column configurations for Product and Category entities

--- APICatalogo/Domain/Category.cs
+++ APICatalogo/Domain/Category.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Domain;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUri { get; set; }
+
     public ICollection<Product>? Products { get; set; } = new Collection<Product>();
 }
